f8c7f8e3195d937f043e3dec89ded72a600601a7
javaabhishek <java.abhishek.patil@gmail.com>
2023-05-10 08:41:56 +0530

    spring6receipt project setup

---
+++ src/main/java/com/asoft/spring6receipe/Spring6receipeApplication.java
@@ -0,0 +1,13 @@
+package com.asoft.spring6receipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6receipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6receipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asoft/spring6receipe/Spring6receipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asoft.spring6receipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6receipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
