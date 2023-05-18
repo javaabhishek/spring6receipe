047360d249fe751883b8e794d153ce4f96e41068
javaabhishek <java.abhishek.patil@gmail.com>
2023-05-18 09:44:33 +0530

    Submitting Unit test for IndexController, Using only JUnit 5 and mockito - Added ArgumentCaptor

--- src/test/java/com/asoft/spring6receipe/controller/IndexControllerUnitTest.java
+++ src/test/java/com/asoft/spring6receipe/controller/IndexControllerUnitTest.java
@@ -4,6 +4,7 @@ import com.asoft.spring6receipe.model.Recipe;
 import com.asoft.spring6receipe.service.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
@@ -29,13 +30,23 @@ class IndexControllerUnitTest {
 
     @Test
     void index() {
-        Recipe recipe=new Recipe();
+        Recipe recipe1=new Recipe();
+        recipe1.setId(1l);
+        Recipe recipe2=new Recipe();
+        recipe2.setId(2l);
         Set<Recipe> recipeSet=new HashSet<>();
-        recipeSet.add(recipe);
+        recipeSet.add(recipe1);
+        recipeSet.add(recipe2);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor=ArgumentCaptor.forClass(Set.class);
+
         when(recipeService.getAllRecipe()).thenReturn(recipeSet);
         String indexOutput=indexController.index(model);
         assertEquals(indexOutput,"index");
         verify(recipeService,times(1)).getAllRecipe();
-        verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+        //verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+        verify(model,times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
+        Set<Recipe> whatIsReturnByRecipeSerivce=argumentCaptor.getValue();
+        assertEquals(2,whatIsReturnByRecipeSerivce.size());
     }
 }
